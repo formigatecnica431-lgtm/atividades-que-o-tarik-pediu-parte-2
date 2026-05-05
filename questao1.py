7dc2c94dcec3eca889723ae369ace076bafea19a
denzel moreira <formigatecnica431@gmail.com>
2026-05-05 18:39:31 -0300

    questao1.py

--- questao1.py
+++ questao1.py
@@ -15,29 +15,53 @@ while True:
         mas as vezes ele se sente sufocado e se combra demais para ajudar os outros pensando mais no proximo
         do que em si....ele tem bom coração.
         """)
+        break
     elif numero == 3 :
         print("""O 3 ele gosta muito do 2 e do 1 eles são um trio e tanto! as vezes ele se sente um terrotado 
         sempre tentando tentando pra no final ser um quaser lá mal sabe ele que sua determinação poucos tem
         além disso ele é bonitão.
         """)
+        break
     elif numero == 4 :
         print("""O 4 é o segundo mais velho, além de ser um baita de um safado sempre de 4, de fato curioso
         pois quando se tem um assundo 18+ pessoas lindam com formas diferente podendo até ser agressivas
         mas o 4 lida com isso de forma muito madura, dando conselhos por conta de suas esperiencias de vida e arrependimentos,
         sempre que precisam falar de um assunto delicado desses o 4 sempre ajuda dando o melhor conselho possivel
         """)
+        break
     elif numero == 5 :
         print("""O 5 o mais interessante apesar de não ser o mais velho do grupo ele trata os outros feito como se fosse um pai,
         pense nele como um tio carismatico aquele que grita come e faz rir aquele trata família de maneira seria sempre
         acolhendo todos com um grande apraço peludo ele é um fofão
         """)
+        break
     elif numero == 6 :
         print("""O 6 anda muito com seu namorado 7 ele juntos 67 são uma dupla de bobões sempre rindo juntos,
         mas o 6 é bem calminho gosta de fazer crochê as vezes ele fala demais reclamando de qualquer coisa
         bem irritante, porém no fundo é um doce que so quer um abraço para lembra que 1 dia ruim não é uma vida ruim.
         """)
+        break
     elif numero == 7 :
         print("""O 7 ele é um nerd animado sempre bricando e falando de rpg, boa parte da sua vida ele não via seu
         prorio sucesso ele estudou pra vairas coisas mas não se interessou por nada, sempre
         peocupado com o futuro até que chegou a conclusão de
         """)
+        break
+    elif numero == 8 :
+        print("""
+        O número 8 ele é um tanto timido mas adora atuar, parece outra pessoa quando está em um palco
+        quando ele sobe lá é como se dissesse olhe para mim com uma bela apresentação, as vezes
+        ele fica tão preocupado com o show que esquece de viver a própria vida.""")
+        break
+    elif numero == 9 :
+        print ("""O 9 ele é bem determinado mas tentar ser alguém que não é, ele se inspira muito no 10
+        e as vezes pensa que é um 10 fazendo tudo que o 10 faz mas ele sempre cai mas por que?""") 
+        break
+    elif numero == 10 :
+        print ("""O 10 o melhor ou pior ? ele sempre faz essa pergunta, teve uma infância dificíl
+        os outros sempre faziam ele se sentir o ultimo lugar o lixo o perdedor engraçado mas até que teve
+        uma boa notícia ele virou professor por e tirou as melhores notas da sua turma.""")
+        break
+    else:
+        print("LÍNGUAGEM ALÍENIGENA DETECTADA ESSE APARELHO IRÁ EXPLODIR EM 3 2 1")
+        break
